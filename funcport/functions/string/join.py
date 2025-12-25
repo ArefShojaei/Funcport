71d16fe8468622ed2757056f6af64683f0f2b06d
ArefShojaei <arefshojaei82@gmail.com>
2025-12-25 18:45:49 +0330

    refactor: join function logic code

--- funcport/functions/string/join.py
+++ funcport/functions/string/join.py
@@ -1,4 +1,6 @@
-def join(separator: str, list: list) -> str:
+from typing import List
+
+def join(separator: str, input: List[str]) -> str:
     """
     @summary Join list elements into a string
 
@@ -10,9 +12,19 @@ def join(separator: str, list: list) -> str:
         >>> join("-", ["a", "b", "c"])
         "a-b-c"
     """
+    if not isinstance(separator, str): raise TypeError("separator should be string!")
+
+    if not isinstance(input, list): raise TypeError("input should be list of string!")
+
+    if len(input) <= 1: raise ValueError("input size can not be less than 2!")
+
+    if not separator: raise ValueError("separator can not be empty!")
+
     text = ""
 
-    for _, word in enumerate(list):
+    for _, word in enumerate(input):
+        if not isinstance(word, str): raise ValueError("each input data should be string!")
+
         text += word + separator
 
     return text[:-len(separator)]
--- tests/unit/functions/string/join_test.py
+++ tests/unit/functions/string/join_test.py
@@ -1,5 +1,6 @@
 import pytest
 from funcport import string
+from typing import Any
 
 
 @pytest.mark.parametrize("wordList, separator, expected", [
@@ -10,3 +11,34 @@ def test_join_list_of_string_to_string(wordList: list, separator: str, expected:
     value = string.join(separator, wordList)
 
     assert value == expected
+
+@pytest.mark.parametrize("separator, input, expected_exception, expected_message", [
+    # Separator -> TypeError
+    (1, ["a", "b", "c"], TypeError, "separator should be string!"),
+    (3.14, ["a", "b", "c"], TypeError, "separator should be string!"),
+    (["list"], ["a", "b", "c"], TypeError, "separator should be string!"),
+    (("tuple",), ["a", "b", "c"], TypeError, "separator should be string!"),
+    ({"dict": []}, ["a", "b", "c"], TypeError, "separator should be string!"),
+    (True, ["a", "b", "c"], TypeError, "separator should be string!"),
+    (False, ["a", "b", "c"], TypeError, "separator should be string!"),
+
+    # # Separator -> ValueError
+    ("", ["a", "b", "c"], ValueError, "separator can not be empty!"),
+
+    # # Input -> TypeError
+    ("*", 1, TypeError, "input should be list of string!"),
+    ("*", 3.14, TypeError, "input should be list of string!"),
+    ("*", ("tuple",), TypeError, "input should be list of string!"),
+    ("*", {"dict": []}, TypeError, "input should be list of string!"),
+    ("*", True, TypeError, "input should be list of string!"),
+    ("*", False, TypeError, "input should be list of string!"),
+
+    # # Input -> ValueError
+    ("*", ["a"], ValueError, "input size can not be less than 2!"),
+    ("*", [1, 2], ValueError, "each input data should be string!"),
+])
+def test_join_validation(separator: Any, input: Any, expected_exception: object, expected_message: str):
+    with pytest.raises(expected_exception) as error:
+        string.join(separator, input)
+
+    assert str(error.value) == expected_message
