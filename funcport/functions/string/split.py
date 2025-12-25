42e738c0385b183394be2a3c101dade701b5defc
ArefShojaei <arefshojaei82@gmail.com>
2025-12-25 16:12:36 +0330

    refactor: refactor split function logic code

--- funcport/functions/string/split.py
+++ funcport/functions/string/split.py
@@ -10,4 +10,8 @@ def split(text: str) -> list:
         >>> split("Hello")
         ["H", "e", "l", "l", "o"]
     """
+    if not isinstance(text, str): raise TypeError("Invalid input data type!")
+
+    if not len(text): return []
+
     return [char for _, char in enumerate(text)]
--- tests/unit/functions/string/split_test.py
+++ tests/unit/functions/string/split_test.py
@@ -1,5 +1,6 @@
 import pytest
 from funcport import string
+from typing import Any
 
 
 @pytest.mark.parametrize("text, expected", [
@@ -7,9 +8,31 @@ from funcport import string
     ("Dev", ["D", "e", "v"]),
     ("Unit test", ["U", "n", "i", "t", " ", "t", "e", "s", "t"])
 ])
-def test_split_string(text: str, expected: list):
-    list = string.split(text)
+def test_split_full_string_to_list(text: str, expected: list):
+    assert string.split(text) == expected
+    assert type(string.split(text)) == type(expected)
 
-    assert list == expected
-    assert type(list) == type(expected)
-    assert list is not expected
+@pytest.mark.parametrize("input", [
+    None, 
+    123, 
+    45.67, 
+    ["list"], 
+    {"dict": 1}, 
+    {"set"}, 
+    b"bytes", 
+    True, 
+    False, 
+    lambda x: x, 
+    object()
+])
+def test_split_string_to_list_with_invalid_input(input: Any):
+    with pytest.raises(TypeError) as error:
+        string.split(input)
+
+    assert str(error.value) == "Invalid input data type!"
+
+def test_split_empty_string_to_list():
+    empty_text = ""
+    
+    assert string.split(empty_text) == []
+    assert type(string.split(empty_text)) == list
