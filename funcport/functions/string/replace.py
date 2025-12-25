aea8f5f499e08ad11537035962639838669189b5
ArefShojaei <arefshojaei82@gmail.com>
2025-12-25 18:14:40 +0330

    refactor: refactor logic code

--- funcport/functions/string/replace.py
+++ funcport/functions/string/replace.py
@@ -1,6 +1,8 @@
 import re
+from typing import Union
 
-def replace(text: str, find: str, replace: str) -> str:
+
+def replace(text: str, find: str, replace: str) -> Union[str, None]:
     """
     @summary Replace substrings in a string
     
@@ -12,4 +14,10 @@ def replace(text: str, find: str, replace: str) -> str:
         >>> replace("Hello world", "world", "Aref")
         "Hello Aref"
     """
+    if not isinstance(text, str): raise TypeError("text should be string!")
+    
+    if not isinstance(find, str): raise TypeError("find should be string!")
+    
+    if not isinstance(replace, str): raise TypeError("replace should be string!")
+
     return re.search(find, text) and re.sub(find, replace, text)
