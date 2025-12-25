1aa0dc53e9d992948a71a16d0f02938c094987de
ArefShojaei <arefshojaei82@gmail.com>
2025-12-25 19:05:21 +0330

    refactor: explode function logic code

--- funcport/functions/string/explode.py
+++ funcport/functions/string/explode.py
@@ -1,6 +1,7 @@
 import re
+from typing import List
 
-def explode(separator: str, text: str) -> list:
+def explode(separator: str, text: str) -> List[str]:
     """
     @summary Split string into list (PHP explode equivalent)
     
@@ -15,4 +16,12 @@ def explode(separator: str, text: str) -> list:
         >>> explode("*", "a*b*c")
         ["a", "b", "c"]
     """
-    return re.split(separator, text)
+    if not isinstance(separator, str): raise TypeError("separator should be string!")
+
+    if not isinstance(text, str): raise TypeError("text should be string!")
+
+    if not separator: raise ValueError("separator can not be empty!")
+
+    escaped_separator = re.escape(separator)
+
+    return re.split(escaped_separator, text)
--- tests/unit/functions/string/explode_test.py
+++ tests/unit/functions/string/explode_test.py
@@ -4,7 +4,8 @@ from funcport import string
 
 @pytest.mark.parametrize("text, separator, expected", [
     ("Hello world", " ", ["Hello", "world"]),
-    ("Python unit test", " ", ["Python", "unit", "test"])
+    ("Python unit test", " ", ["Python", "unit", "test"]),
+    ("Back-end", " ", ["Back-end"])
 ])
 def test_explode_string_to_list(text: str, separator: str, expected: list):
     list = string.explode(separator, text)
